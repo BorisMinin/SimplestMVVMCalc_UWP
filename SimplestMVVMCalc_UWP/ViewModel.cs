197f91ca792667832381de6e2263e71b26bc7833
Борис Минин <boris.minin@outlook.com>
2018-03-29 22:49:49 +0300

    Implemented interface INotifyPropertyChanged and added variables

--- SimplestMVVMCalc_UWP/ViewModel.cs
+++ SimplestMVVMCalc_UWP/ViewModel.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace SimplestMVVMCalc_UWP
 {
-    class ViewModel
+    class ViewModel : INotifyPropertyChanged
     {
+        #region Объявление переменных
+        string op1;
+        string op2;
+        string result;
+        #endregion
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
     }
 }
