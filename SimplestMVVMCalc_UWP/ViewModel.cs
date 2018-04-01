9f63cf9112b4c72f5dfb8fce66b2354cdc96b4e4
Борис Минин <boris.minin@outlook.com>
2018-04-01 15:09:48 +0300

    Completely finished class ViewModel

--- SimplestMVVMCalc_UWP/ViewModel.cs
+++ SimplestMVVMCalc_UWP/ViewModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
+//1
 
 namespace SimplestMVVMCalc_UWP
 {
@@ -11,9 +12,9 @@ namespace SimplestMVVMCalc_UWP
     {
            
         #region Объявление переменных
-        string op1; // переменная для хранения первого операнда
-        string op2; // переменная для хранения второго операнда
-        string result; // переменная для вывд ответа
+        string _op1; // переменная для хранения первого операнда
+        string _op2; // переменная для хранения второго операнда
+        string _result; // переменная для вывд ответа
         #endregion
 
         #region Свойства команды
@@ -22,19 +23,78 @@ namespace SimplestMVVMCalc_UWP
 
         #region Реализация интерфейса INotifyPropertyChanged
         public event PropertyChangedEventHandler PropertyChanged;
-        //public void OnPropertyChanged(string propertyName)
-        //{
-        //    if (PropertyChanged != null)
-        //        PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-        //}
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        #region Свойства привязки
+        public string Operand1
+        {
+            get { return _op1; }
+            set
+            {
+                _op1 = value;
+                CalcCommand.RaiseCanExecuteChanged();
+            }
+        }
+        public string Operand2
+        {
+            get { return _op2; }
+            set
+            {
+                _op2 = value;
+                CalcCommand.RaiseCanExecuteChanged();
+            }
+        }
+        public string Result
+        {
+            get { return _result; }
+            set
+            {
+                _result = value;
+                OnPropertyChanged("Result");
+            }
+        }
+
+
+        #endregion
+
+        #region Логика команды
+        public void CalcCommandExecute(object parametr)
+        {
+            switch (parametr)
+            {
+                case "+":
+                    Result = (Double.Parse(_op1) + Double.Parse(_op2)).ToString();
+                    break;
+                case "-":
+                    Result = (Double.Parse(_op1) - Double.Parse(_op2)).ToString();
+                    break;
+                case "*":
+                    Result = (Double.Parse(_op1) * Double.Parse(_op2)).ToString();
+                    break;
+                case "/":
+                    Result = (Double.Parse(_op1) / Double.Parse(_op2)).ToString();
+                    break;
+            }
+        }
+        public bool CanExecuteCalcCommand()
+        {
+            if (String.IsNullOrEmpty(_op1) || String.IsNullOrEmpty(_op2))
+                return false;
+            else
+                return true;
+        }
         #endregion
 
         #region Конструктор класса ViewModel
         public ViewModel()
         {
-            
+            CalcCommand = new RelayCommand(CalcCommandExecute, CanExecuteCalcCommand);
         }
         #endregion
-
     }
 }
